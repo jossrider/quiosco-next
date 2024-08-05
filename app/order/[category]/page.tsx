356e2674f2f29fc008419b5768e712edb618b68a
Joss Rider <jossrider@outlook.com>
2024-08-04 21:41:09 -0400

    Seguimos agergando funcionalidades..

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,4 +1,5 @@
 import ProductCard from '@/components/products/ProductCard'
+import Heading from '@/components/ui/Heading'
 import { prisma } from '@/src/lib/prisma'
 
 async function getProducts(category: string) {
@@ -13,7 +14,7 @@ export default async function OrderPage({ params }: { params: { category: string
   const products = await getProducts(params.category)
   return (
     <>
-      <h1 className='text-2xl my-10 font-bold text-center'>Elige y personaliza tu pedido a continuacion</h1>
+      <Heading>Elige y personaliza tu pedido a continuacion</Heading>
       <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start'>
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
