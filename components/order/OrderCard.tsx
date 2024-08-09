ed6f100c1c40d282971a9a0dee90edfef9837467
Joss Rider <jossrider@outlook.com>
2024-08-09 19:47:35 -0400

    mas codigo agregado

--- components/order/OrderCard.tsx
+++ components/order/OrderCard.tsx
@@ -1,5 +1,6 @@
 import { OrderWithProducts } from '@/src/types'
 import { formatCurrency } from '@/src/utils'
+import { completeOrder } from '@/actions/complete-order-action'
 
 type OrderCardProps = {
   order: OrderWithProducts
@@ -29,7 +30,8 @@ export default function OrderCard({ order }: OrderCardProps) {
         </div>
       </dl>
 
-      <form>
+      <form action={completeOrder}>
+        <input type='hidden' value={order.id} name='order_id' />
         <input
           type='submit'
           className='bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer'
