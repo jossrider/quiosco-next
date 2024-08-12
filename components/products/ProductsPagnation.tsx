15f02dbc8c36e9f366c5656bdf55dcd86ff62d22
Joss Rider <jossrider@outlook.com>
2024-08-11 21:58:23 -0400

    Agregamos formulario de registro de imagenes

--- components/products/ProductsPagnation.tsx
+++ components/products/ProductsPagnation.tsx
@@ -18,6 +18,7 @@ export default function ProductsPagnation({ page, totalPages }: ProductsPagnatio
       )}
       {pages.map((currentPage) => (
         <Link
+          key={currentPage}
           href={`/admin/products?page=${currentPage}`}
           className={`${page === currentPage && 'font-black'}
           bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0`}>
